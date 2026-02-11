53bfab290796f6a8914ca0db0242786d602ac032
‪Elsayed Mustafa‬‏ <110793510+El-Sayed-Mustafa@users.noreply.github.com>
2026-02-11 21:52:10 +0200

    updata projects view

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -63,7 +63,7 @@ export default function Projects() {
           </p>
         </div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-8">
           {projects.map((project, index) => {
             const Icon = project.icon;
             return (
