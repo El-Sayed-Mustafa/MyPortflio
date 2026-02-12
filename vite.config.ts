8fbb74329bdd8443a9be4b5ccfbfa763684a14da
‪Elsayed Mustafa‬‏ <110793510+El-Sayed-Mustafa@users.noreply.github.com>
2026-02-12 11:09:40 +0200

    Error

--- vite.config.ts
+++ vite.config.ts
@@ -2,3 +2,14 @@ export default defineConfig({
   base: '/MyPortflio/', // اكتب اسم المستودع هنا بالظبط بين الـ slashes
   plugins: [react()], // أو أي بلجن تستخدمه
 })
+
+function react() {
+  throw new Error("Function not implemented.");
+}
+function defineConfig(arg0: {
+  base: string; // اكتب اسم المستودع هنا بالظبط بين الـ slashes
+  plugins: void[];
+}) {
+  throw new Error("Function not implemented.");
+}
+
