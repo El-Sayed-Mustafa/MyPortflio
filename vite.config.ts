a8ba49a6d6ce6cb9e60691b3c29e724d3441cb56
‪Elsayed Mustafa‬‏ <110793510+El-Sayed-Mustafa@users.noreply.github.com>
2026-02-12 11:06:13 +0200

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/MyPortflio/', // اكتب اسم المستودع هنا بالظبط بين الـ slashes
+  plugins: [react()], // أو أي بلجن تستخدمه
+})
